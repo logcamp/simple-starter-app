aeac511c9b6a54e98362b02b3a53f3e209e916ee
logcamp <robert@logcamp.org>
2014-07-24 09:18:26 -0700

    Committed initial files

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/cnanders/pen/kouBs.
+
+ Demo showing how to wire up side menus along with navigation within Ionic.
---
+++ js/app.js
@@ -0,0 +1,62 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('page1', {
+    url: "/page1",
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/page1.html'
+      }
+    }
+  })
+  .state('page2', {
+    url: "/page2",
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/page2.html'
+      }
+    }
+  })
+  .state('page3', {
+    url: "/page3",
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/page3.html'
+      }
+    }
+  })
+  .state('page4', {
+    url: "/page4",
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/page4.html'
+      }
+    }
+  })
+  .state('page5', {
+    url: "/page5",
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/page5.html'
+      }
+    }
+  })
+  .state('page6', {
+    url: "/page6",
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/page6.html'
+      }
+    }
+  })
+  $urlRouterProvider.otherwise('/page1');
+  
+})
+
+.controller('MenuCtrl', function($scope) {
+  $scope.openLeft = function() {
+    $scope.sideMenuController.toggleLeft();
+    console.log($scope);
+  };
+});
